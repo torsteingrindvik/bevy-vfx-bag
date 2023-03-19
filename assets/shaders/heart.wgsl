1f832b44279dd979b43f74972c48c700c352138f
Torstein Grindvik <torstein.grindvik@nordicsemi.no>
2023-03-19 23:57:50 +0100

    Wip, transitions etc.
    Signed-off-by: Torstein Grindvik <torstein.grindvik@nordicsemi.no>

--- assets/shaders/heart.wgsl
+++ assets/shaders/heart.wgsl
@@ -5,7 +5,23 @@
 var<uniform> color: vec4<f32>;
 
 @group(1) @binding(1)
-var<uniform> num_hearts: vec2<f32>;
+var<uniform> num_hearts: f32;
+
+struct Mouse {
+    x: f32,
+    y: f32,
+};
+@group(1) @binding(2)
+var<uniform> mouse: Mouse;
+
+struct HeartData {
+    opacity: f32,
+    scale: f32,
+    angle: f32,
+	_padding: f32,
+};
+@group(1) @binding(3)
+var<uniform> heart_data: array<HeartData, 32>;
 
 fn heart(
 	uv: vec2<f32>
@@ -18,20 +34,286 @@ fn heart(
 
 	uv.y -= 0.2;
 	uv.y += 0.7 * sqrt(abs(uv.x));
-	uv *= vec2(0.9, 1.5);
+	uv *= vec2(0.8, 1.5);
 
 	let d = smoothstep(1.0, 0.99, length(uv));
 
 	return d;
 }
 
+// @fragment
+// fn fragment(
+//     #import bevy_pbr::mesh_vertex_output
+// ) -> @location(0) vec4<f32> {
+// 	let p = fract(vec2(uv.x * num_hearts.x, uv.y * num_hearts.y));
+// 	let h = heart(p);
+// 	let col = h * color.rgb;
+
+//     return vec4(col, h * color.a * 0.5);
+// }
+
+const FLOOR: f32 = 1.0;
+const THING: f32 = 2.0;
+
+// https://iquilezles.org/articles/smin/
+fn op_union(object_1: f32, object_2: f32) -> f32 {
+	let k = 0.13;
+	let h = max(k - abs(object_1 - object_2), 0.0) / k;
+
+	return min(object_1, object_2) - h * h * k * (1./4.);
+}
+
+// fn box(p: vec3<f32>, b: vec3<f32>) -> f32 {
+//   let q = abs(p) - b;
+//   return length(max(q, vec3(0.))) + min(max(q.x, max(q.y, q.z)), 0.);
+// }
+
+// fn weird_ball(p: vec3<f32>, radius: f32) -> f32 {
+// 	let symm = abs(p);
+// 	// let xabs = abs(p.x);
+// 	// let xabs = p.x;
+
+// 	// let py = p.y * 1.3 - xabs * sqrt(((10. - xabs) / 30.));
+
+// 	// let p = vec3(p.x, p.y - pow(pabs, 1.3), p.z);
+// 	// let p = vec3(p.x, py, p.z * (1. - (posy / 10.)));
+
+// 	return length(p + 0.2) - radius;
+// }
+
+fn repeat_xz(p: vec3<f32>, factor: vec2<f32>) -> vec3<f32> {
+	let tmp = vec2(
+        p.x - factor.x * floor(p.x/factor.x),
+        p.z - factor.y * floor(p.z/factor.y)
+    ) - 0.5 * factor;
+    
+	return vec3(tmp.x, p.y, tmp.y);
+}
+
+fn repeat2(p: vec2<f32>, factor: vec2<f32>) -> vec2<f32> {
+	return vec2(
+        p.x - factor.x * floor(p.x/factor.x),
+        p.y - factor.y * floor(p.y/factor.y)
+    ) - 0.5 * factor;
+}
+
+fn ball(p: vec3<f32>, radius: f32) -> f32 {
+	// var p = vec3(p.x, p.y / 2., p.z + 1.3 * abs(p.x));
+
+	// let s = abs(p / 1.1);
+
+	return length(p) - radius;
+}
+
+fn abschill(v: f32) -> f32 {
+	// When v is big the extra term is negligible.
+	// When v is small it smooths it out.
+	return sqrt(v*v + 0.005);
+}
+
+fn map(p: vec3<f32>) -> vec2<f32> {
+	let t = 0.;
+
+	// var p = repeat_xz(p, vec2(5., 5.));
+	// p.y *= 0.8;
+
+	// let p = vec3(
+	// 	abschill(p.x) % 2.0,
+	// 	p.y,
+	// 	abschill(p.z) % 2.0
+	// );
+
+	// var res = vec2(
+	// 	ball(p - vec3(0., 1.2, 0.), 0.2),
+	// 	THING,
+	// );
+
+	let height = 0.4;
+	let y = sqrt(max(0.0, p.y)) - 0.2;
+
+	var res = vec2(
+		ball(vec3(abschill(p.x * 0.8) - y * 0.5, -height + p.y * 0.8, p.z), 0.3),
+		THING
+	);
+
+	// floor
+	let fl = 0.1 + p.y;
+	if (fl < res.x) {
+		// res = vec2(fl, FLOOR);
+	};
+
+	return res;
+}
+
+fn normal(p: vec3<f32>) -> vec3<f32> {
+	let eps = vec3(0.0001, 0., 0.);
+
+	return normalize(vec3(
+		map(p + eps.xyy).x - map(p - eps.xyy).x,
+		map(p + eps.yxy).x - map(p - eps.yxy).x,
+		map(p + eps.yyx).x - map(p - eps.yyx).x
+	));
+}
+
+// Returns (distance, material id)
+fn ray_march(
+	ro: vec3<f32>,
+	rd: vec3<f32>,
+) -> vec2<f32> {
+	let eps = 1e-3;
+
+	var t = 0.5;
+	let t_max = 20.0;
+
+	var res = vec2(-1.);
+
+	for (var i: i32 = 0; i < 500 && t < t_max; i++) {
+		let m = map(ro + rd * t);
+
+		if (m.x < eps) {
+			res = vec2(t, m.y);
+			break;
+		}
+
+		t += m.x;
+	}
+	if t > t_max {
+		res = vec2(-1.);
+	}
+
+	return res;
+}
+
+// Make p from -1 to 1, aspect independent, 
+// bottom left (-1, -1) towards top right (1, 1)
+fn uv2p(uv: vec2<f32>, view: View) -> vec3<f32> {
+	var uv = vec2(uv.x, 1.0 - uv.y);
+	uv.x *= num_hearts;
+	let uvxi = floor(uv.x);
+	uv.x = uv.x % 1.0;
+	// uv.y = uv.y * 4.8;
+
+	var p = (uv * 2.) - 1.;
+
+	let width = view.viewport.z;
+	let height = view.viewport.w;
+
+	// aspect ratio independent (assume width > height)
+	p.x *= width / height;
+
+	return vec3(p, uvxi);
+}
+
 @fragment
 fn fragment(
     #import bevy_pbr::mesh_vertex_output
 ) -> @location(0) vec4<f32> {
-	let p = fract(vec2(uv.x * num_hearts.x, uv.y * num_hearts.y));
-	let h = heart(p);
-	let col = h * color.rgb;
+	// let uv = repeat2(uv, vec2(1.0));
+	let p_idx = uv2p(uv, view);
+	var p = p_idx.xy;
+	let idx = u32(p_idx.z);
+	let hd = heart_data[idx];
 
-    return vec4(col, h * color.a);
+	let pi = 3.1415;
+
+	// let angle = 0.1 * sin(globals.time * 5.) + 1.2 + mouse.x;
+	let angle = (1. - hd.angle) * pi + 1.4;
+
+	let r = 2.4;
+
+	let ta = vec3(0., 0.65, 0.4);
+	let ro = ta + vec3(r * cos(angle), -0.2, r * sin(angle));
+
+	let lo = vec3(1., 1., 1.);
+	// light: direction is towards sun as seen from target
+	let ld = normalize(lo);
+	let light_color = vec3(0.6, 0.35, 0.5);
+
+	// camera looks from ro towards ta
+	let cam_look = normalize(-ro + ta);
+	let world_up = vec3(0., 1., 0.);
+
+	let cam_right = normalize(cross(cam_look, world_up));
+	let cam_up = normalize(cross(cam_right, cam_look));
+
+	// let zoom = 1.8 + 3. * interp;
+	let zoom = hd.scale * 4.0;
+
+	// p = repeat2(p, vec2(2., 2.));
+
+	let rd = normalize(p.x * cam_right + p.y * cam_up + zoom * cam_look);
+
+	let rm = ray_march(
+		ro,
+		rd,
+	);
+	let t = rm.x;
+	// Add a bit to avoid float comparison issues
+	let id = rm.y + 0.1;
+
+	// var col = vec3(0.0);
+	var col = vec3(0.4, 0.7, 0.9) - vec3(0.5) * max(p.y, 0.);
+	var a = 0.0;
+
+	if (t > 0.0) {
+		if (id > THING) {
+			col = vec3(0.8, 0.02, 0.02);
+
+			a = hd.opacity;
+		} else if (id > FLOOR) {
+			col = vec3(0.05, 0.09, 0.02);
+		}
+
+		let point = ro + rd * t;
+		let n = normal(point);
+
+		let sun_norm_alignment = max(0., dot(n, ld));
+
+		let view = -rd;
+		let sun_half_vector = normalize(view + ld);
+
+		// 1.0 if can see sun directly,
+		// 0.0 else
+		let sun_unobstructed = step(ray_march(
+			point + 0.001 * n,
+			ld,
+		).y, 0.);
+
+		let specular_alignment = max(0.0, dot(n, sun_half_vector));
+		let specular = pow(specular_alignment, 32.) * sun_norm_alignment;
+
+		let sky_factor = sqrt(clamp(0.5 + 0.5 * n.y, 0.0, 1.0));
+
+		let bottom_up_factor = pow(clamp(0.3 - 0.9 * n.y, 0.0, 0.5), 2.);
+
+		var light_in = vec3(0.0);
+
+		// create a color for the sun, adjust amplitude by alignment of
+		// geometry, then forget all of it if the current point is not visible
+		// to the sun.
+		let sun_color = vec3(9., 6., 4.);
+		light_in += sun_color * sun_norm_alignment * sun_unobstructed;
+
+		// add some color based on the sky, adjust amplitude by how aligned
+		// the geometry is to the sky.
+		light_in += vec3(0.5, 0.7, 1.) * sky_factor;
+
+		light_in += vec3(0.4, 1., 0.4) * bottom_up_factor;
+
+		// now everything is scaled by this..
+		col *= light_in;
+
+		// specular comes in last?
+		col += sun_color * specular * sun_unobstructed;
+
+
+		col = mix(col, vec3(0.5, 0.6, 0.9), 1. - exp(-0.0001 * t * t * t));
+
+		// col = vec3(bottom_up_factor);
+		// a = 1.0;
+		// a = heart_data[u32(idx)].opacity;
+	}
+
+    return vec4(col, a);
+    // return vec4(col, 1.);
 }
--- examples/hearts/main.rs
+++ examples/hearts/main.rs
@@ -1,13 +1,16 @@
 #[path = "../examples_common.rs"]
 mod examples_common;
 
+// TODO: https://github.com/bevyengine/bevy/issues/6754
+// Try bloom UI
+
+// use std::default;
+
 use bevy::{
-    core_pipeline::{
-        bloom::BloomSettings, clear_color::ClearColorConfig, tonemapping::Tonemapping,
-    },
-    prelude::*,
+    core_pipeline::clear_color::ClearColorConfig,
+    prelude::{shape::Quad, *},
     reflect::TypeUuid,
-    render::render_resource::{AsBindGroup, ShaderRef},
+    render::render_resource::{AsBindGroup, ShaderRef, ShaderType},
     sprite::{Material2d, Material2dPlugin, MaterialMesh2dBundle, Mesh2dHandle},
 };
 
@@ -17,6 +20,10 @@ fn main() {
         .add_plugin(examples_common::ShapesExamplePlugin::default())
         .add_plugin(Material2dPlugin::<HeartMaterial>::default())
         .add_startup_system(setup)
+        .add_system(update_mouse)
+        .add_system(update_heart_materials)
+        .add_system(update_keyboard.before(update_heart_materials))
+        .add_system(update_heart_quads.after(update_heart_materials))
         .run();
 }
 
@@ -24,55 +31,40 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<HeartMaterial>>,
-    windows: Query<&Window>,
 ) {
-    let window = windows.single();
-
-    let hearts_quad_1 = shape::Quad::new(Vec2::new(
-        (window.physical_width() - 300) as f32,
-        (window.physical_height() - 150) as f32,
-    ));
-    let hearts_color_1 = Color::Rgba {
-        red: 0.9,
-        green: 0.,
-        blue: 0.,
-        alpha: 0.9,
-    };
+    let hearts_quad_1 = shape::Quad::default();
     let hearts_transform_1 = Transform::from_xyz(0., 0., 0.);
 
     commands.spawn(MaterialMesh2dBundle {
         mesh: Mesh2dHandle(meshes.add(hearts_quad_1.into())),
-        material: materials.add(HeartMaterial {
-            color: hearts_color_1,
-            num_hearts: Vec2::new(5., 2.),
-        }),
+        material: materials.add(HeartMaterial::new(Color::RED, 256., 6)),
         transform: hearts_transform_1,
         ..default()
     });
 
-    let hearts_quad_2 = shape::Quad::new(Vec2::new(
-        (window.physical_width() / 10) as f32,
-        (window.physical_height() / 4) as f32,
-    ));
-    let hearts_color_2 = Color::Rgba {
-        red: 0.1,
-        green: 0.,
-        blue: 0.9,
-        alpha: 0.9,
-    };
-    let hearts_transform_2 = Transform::from_xyz(0., 0., 1.);
+    // let hearts_quad_2 = shape::Quad::new(Vec2::new(
+    //     (window.physical_width() / 10) as f32,
+    //     (window.physical_height() / 4) as f32,
+    // ));
+    // let hearts_color_2 = Color::Rgba {
+    //     red: 0.1,
+    //     green: 0.,
+    //     blue: 0.9,
+    //     alpha: 0.9,
+    // };
+    // let hearts_transform_2 = Transform::from_xyz(0., 0., 1.);
 
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: Mesh2dHandle(meshes.add(hearts_quad_2.into())),
-        material: materials.add(HeartMaterial {
-            color: hearts_color_2,
-            num_hearts: Vec2::new(4., 4.),
-        }),
-        transform: hearts_transform_2,
-        ..default()
-    });
+    // commands.spawn(MaterialMesh2dBundle {
+    //     mesh: Mesh2dHandle(meshes.add(hearts_quad_2.into())),
+    //     material: materials.add(HeartMaterial {
+    //         color: hearts_color_2,
+    //         num_hearts: Vec2::new(4., 4.),
+    //     }),
+    //     transform: hearts_transform_2,
+    //     ..default()
+    // });
 
-    commands.spawn((Camera2dBundle {
+    commands.spawn(Camera2dBundle {
         camera: Camera {
             order: 1,
             ..default()
@@ -81,7 +73,7 @@ fn setup(
             clear_color: ClearColorConfig::None,
         },
         ..default()
-    },));
+    });
 }
 
 impl Material2d for HeartMaterial {
@@ -90,12 +82,332 @@ impl Material2d for HeartMaterial {
     }
 }
 
+#[derive(Debug, ShaderType, Clone, Default, Copy, Component)]
+struct HeartData {
+    // fade in/out
+    opacity: f32,
+
+    // scale in/out
+    scale: f32,
+
+    // spin in/out
+    angle: f32,
+
+    _padding: f32,
+}
+
+impl HeartData {
+    fn transition_to_percentage(style: TransitionStyle, percentage: f32) -> Self {
+        match style {
+            TransitionStyle::Instant => Self {
+                opacity: percentage,
+                scale: percentage,
+                angle: percentage,
+                ..default()
+            },
+            TransitionStyle::Fade => Self {
+                opacity: percentage,
+                scale: 1.0,
+                angle: 1.0,
+                ..default()
+            },
+            TransitionStyle::Scale => Self {
+                opacity: 1.0,
+                scale: percentage,
+                angle: 1.0,
+                ..default()
+            },
+            TransitionStyle::Spin => Self {
+                opacity: 1.0,
+                scale: 1.0,
+                angle: percentage,
+                ..default()
+            },
+        }
+    }
+}
+
+#[derive(Debug, Clone, Default, Copy)]
+struct HeartSettings {
+    transition: Option<Transition>,
+}
+
 #[derive(AsBindGroup, TypeUuid, Debug, Clone)]
 #[uuid = "ff664fca-c02f-11ed-bf9f-325096b39f47"]
-pub struct HeartMaterial {
+pub struct HeartMaterial<const N: usize = 32> {
     #[uniform(0)]
     color: Color,
 
     #[uniform(1)]
-    num_hearts: Vec2,
+    num_hearts: f32,
+
+    #[uniform(2)]
+    mouse: Vec2,
+
+    // TODO: Conditionally use storage buffers
+    #[uniform(3)]
+    hearts: [HeartData; N],
+
+    heart_settings: [HeartSettings; N],
+
+    size: f32,
+}
+
+#[derive(Debug, Clone, Default, Copy)]
+pub enum TransitionStyle {
+    #[default]
+    Instant,
+
+    Fade,
+    Scale,
+    Spin,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct TransitionSettings {
+    speed: f32,
+    style: TransitionStyle,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Transition {
+    transitioning_in: bool,
+    transition_percentage: f32,
+
+    settings: TransitionSettings,
+}
+
+impl Transition {
+    fn is_done(&self) -> bool {
+        if self.transitioning_in {
+            self.transition_percentage >= 1.0
+        } else {
+            self.transition_percentage <= 0.0
+        }
+    }
+}
+
+impl<const N: usize> HeartMaterial<N> {
+    pub fn new(color: Color, size: f32, num_hearts: usize) -> Self {
+        assert!(num_hearts <= 32);
+
+        let mut hearts = [Default::default(); N];
+
+        for heart in hearts.iter_mut().take(num_hearts) {
+            *heart = HeartData {
+                opacity: 1.0,
+                scale: 1.0,
+                angle: 1.0,
+                _padding: 0.0,
+            }
+        }
+
+        Self {
+            color,
+            num_hearts: num_hearts as f32,
+            mouse: Default::default(),
+            // interp: Default::default(),
+            hearts,
+            heart_settings: [Default::default(); N],
+            size,
+        }
+    }
+
+    pub fn add_heart(&mut self, settings: TransitionSettings) {
+        let num = self.num_hearts as usize;
+
+        if num == 32 {
+            eprintln!("Too many");
+            return;
+        }
+
+        let (heart_settings, data) = (&mut self.heart_settings[num], &mut self.hearts[num]);
+
+        heart_settings.transition = Some(Transition {
+            transitioning_in: true,
+            transition_percentage: 0.0,
+            settings,
+        });
+
+        *data = HeartData::transition_to_percentage(settings.style, 0.0);
+
+        self.num_hearts += 1.;
+    }
+
+    pub fn remove_heart(&mut self, settings: TransitionSettings) {
+        let num = self.num_hearts as usize;
+
+        if num == 0 {
+            eprintln!("Too few");
+            return;
+        }
+
+        self.num_hearts -= 1.;
+        let heart_settings = &mut self.heart_settings[num];
+
+        let previous_transition_percentage = heart_settings
+            .transition
+            .map(|hs| hs.transition_percentage)
+            .unwrap_or(1.0);
+
+        heart_settings.transition = Some(Transition {
+            transitioning_in: false,
+            transition_percentage: previous_transition_percentage,
+            settings,
+        });
+    }
+}
+
+fn update_mouse(
+    query: Query<&Handle<HeartMaterial>>,
+    mut hearts: ResMut<Assets<HeartMaterial>>,
+    mut cursor_moved_events: EventReader<CursorMoved>,
+    mouse_button_input: Res<Input<MouseButton>>,
+    windows: Query<&Window>,
+) {
+    if mouse_button_input.pressed(MouseButton::Left) {
+        for event in cursor_moved_events.iter() {
+            let window = windows.single();
+
+            for handle in query.iter() {
+                if let Some(heart) = hearts.get_mut(handle) {
+                    heart.mouse.x = event.position.x / window.resolution.width();
+                    heart.mouse.y = event.position.y / window.resolution.height();
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug, Deref)]
+struct Ease(CubicSegment<Vec2>);
+
+impl Default for Ease {
+    fn default() -> Self {
+        // https://docs.rs/bevy/latest/bevy/math/cubic_splines/struct.CubicSegment.html#method.new_bezier
+        Self(CubicSegment::new_bezier((0.25, 0.1), (0.25, 1.0)))
+    }
+}
+
+fn update_heart_materials(
+    query: Query<&Handle<HeartMaterial>>,
+    mut hearts: ResMut<Assets<HeartMaterial>>,
+    curve: Local<Ease>,
+    time: Res<Time>,
+) {
+    let dt = time.delta_seconds();
+
+    for handle in query.iter() {
+        if let Some(heart_material) = hearts.get_mut(handle) {
+            let hearts = &mut heart_material.hearts;
+            let material_settings = &mut heart_material.heart_settings;
+
+            for (index, heart_settings) in material_settings.iter_mut().enumerate() {
+                if let Some(transition) = heart_settings.transition.as_mut() {
+                    transition.transition_percentage = match transition.settings.style {
+                        TransitionStyle::Instant => {
+                            if transition.transitioning_in {
+                                1.0
+                            } else {
+                                0.0
+                            }
+                        }
+                        _others => {
+                            if transition.transitioning_in {
+                                transition.transition_percentage + dt * transition.settings.speed
+                            } else {
+                                transition.transition_percentage - dt * transition.settings.speed
+                            }
+                        }
+                    };
+
+                    let ease = curve.ease(transition.transition_percentage);
+                    println!(
+                        "Updating index {index} to {ease:.4} ({:.4})",
+                        transition.transition_percentage
+                    );
+
+                    hearts[index] =
+                        HeartData::transition_to_percentage(transition.settings.style, ease);
+
+                    if transition.is_done() {
+                        println!("T #{index} done");
+
+                        let _ = heart_settings.transition.take();
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn update_heart_quads(
+    // TODO: Changed
+    query: Query<(&Handle<HeartMaterial>, &Mesh2dHandle)>,
+    hearts: Res<Assets<HeartMaterial>>,
+    mut quads: ResMut<Assets<Mesh>>,
+) {
+    for (heart_handle, mesh_handle) in query.iter() {
+        if let Some(heart_material) = hearts.get(heart_handle) {
+            let mesh = quads.get_mut(&mesh_handle.0).unwrap();
+
+            *mesh = shape::Quad::new(Vec2::new(
+                heart_material.size * heart_material.num_hearts,
+                heart_material.size,
+            ))
+            .into();
+        }
+    }
+}
+
+fn update_keyboard(
+    keyboard_input: Res<Input<KeyCode>>,
+    query: Query<&Handle<HeartMaterial>>,
+    mut hearts: ResMut<Assets<HeartMaterial>>,
+) {
+    let heart_handle = query.single();
+    let heart_material = hearts.get_mut(heart_handle).unwrap();
+    let speed = 0.8;
+
+    if keyboard_input.just_pressed(KeyCode::Q) {
+        heart_material.add_heart(TransitionSettings {
+            speed,
+            style: TransitionStyle::Instant,
+        });
+    } else if keyboard_input.just_pressed(KeyCode::W) {
+        heart_material.add_heart(TransitionSettings {
+            speed,
+            style: TransitionStyle::Fade,
+        });
+    } else if keyboard_input.just_pressed(KeyCode::E) {
+        heart_material.add_heart(TransitionSettings {
+            speed,
+            style: TransitionStyle::Scale,
+        });
+    } else if keyboard_input.just_pressed(KeyCode::R) {
+        heart_material.add_heart(TransitionSettings {
+            speed,
+            style: TransitionStyle::Spin,
+        });
+    } else if keyboard_input.just_pressed(KeyCode::A) {
+        heart_material.remove_heart(TransitionSettings {
+            speed,
+            style: TransitionStyle::Instant,
+        });
+    } else if keyboard_input.just_pressed(KeyCode::S) {
+        heart_material.remove_heart(TransitionSettings {
+            speed,
+            style: TransitionStyle::Fade,
+        });
+    } else if keyboard_input.just_pressed(KeyCode::D) {
+        heart_material.remove_heart(TransitionSettings {
+            speed,
+            style: TransitionStyle::Scale,
+        });
+    } else if keyboard_input.just_pressed(KeyCode::F) {
+        heart_material.remove_heart(TransitionSettings {
+            speed,
+            style: TransitionStyle::Spin,
+        });
+    }
 }
